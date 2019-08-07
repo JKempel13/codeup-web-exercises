aaa9eee5ef92f17e979e5da6d380eee9a203f2b2
Jason Kempel <kempel.jason13@gmail.com>
2019-08-07 16:03:49 -0500

    conditionals-lec

---
+++ js/conditionals-lec.js
@@ -0,0 +1,133 @@
+"use strict";
+
+//
+// var isRaining = true;
+//
+//
+// if (isRaining) {
+//     // Show rain icon
+//     alert("It's raining outside!");
+// }
+//
+//
+// var zeroLives = 0;
+//
+// if (zeroLives) {
+//    alert("GAME OVER")
+// }
+
+// // if/else example
+// if (condition) {
+//     // code here gets executed if condition evaluates to true
+// } else {
+//     // code here gets executed if condition evaluates to false
+// }
+
+// var weShouldDeleteStuff = confirm('Are you sure you want to delete everything?');
+//
+// console.log("The value of the confirm is: " + weShouldDeleteStuff);
+//
+// if (weShouldDeleteStuff) {
+//     // delete all the things...
+//     alert("We are deleting everything...")
+// } else {
+//     alert("Operation Canceled!");
+// }
+
+// var accountIsEmpty = confirm("Do you have money?");
+//
+// if (accountIsEmpty) {
+//     alert("You can pull money!")
+// } else {
+//     alert("You are broke!")
+// }
+
+// var pizzaPreference = prompt("What kind of pizza do you like?");
+//
+// if (pizzaPreference === "pineapple and hot sauce") {
+//     alert("What a coincidence, that's my favorite!");
+// } else if (pizzaPreference === "cheese") {
+//     alert("Just plain cheese? Okay...");
+// } else if (pizzaPreference === "ham") {
+//     alert("ham is easy to spell and awesome!");
+// } else {
+//     alert(pizzaPreference + " isn't my favorite, but let's order some!");
+// }
+
+// function streetLightColor (color1, color2, color3) {
+// }
+
+// if (streetLightColor === "Red") {
+//     alert("STOP");
+// } else if (streetLightColor === "Yellow") {
+//     alert("Slow Down");
+// } else {
+//     alert("Proceed");
+// }
+//
+//
+//
+// //
+// function evaluateIntersectionLights (lightColor) {
+//
+//     if (evaluateIntersectionLights === "Green") {
+//         return "Keep driving...go go go!";
+//     } else if (evaluateIntersectionLights === "Yellow") {
+//         return "Slow down!";
+//     } else if (evaluateIntersectionLights === "Red") {
+//         return "STOP!";
+//     } else {
+//         return "Proceed with Caution";
+//     }
+// }
+//
+// console.log(evaluateIntersectionLights("Red"));
+
+
+// function evaluateIntersectionLights (lightColor) {
+//
+//
+// switch (lightColor) {
+//     case "Green":
+//          "Keep driving...go go go!";
+//     break;
+//     case "Yellow":
+//         return "Slow Down;"
+//
+// }
+
+//If the "sunny" weather option is selected, the response reads: "It is nice and sunny outside today. Wear shorts! Go to the beach, or the park, and get an ice cream."
+
+// If the "Rainy" weather option is selected, the response reads: "
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
