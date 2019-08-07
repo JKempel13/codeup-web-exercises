8d3e487dd266397b15b44ed6e68c63635b7359e8
Jason Kempel <kempel.jason13@gmail.com>
2019-08-07 13:30:43 -0500

    functions bonus work

---
+++ js/function-bonus.js
@@ -0,0 +1,28 @@
+"use strict";
+
+// function isMultipleOf (target, n) {
+//     return target % n;
+// }
+//
+// console.log (isMultipleOf(6, 3));
+//
+// // function hasLowerCase (string) {
+// //     return
+// // }
+//
+// function invertSign (num) {
+//     return num * -1
+// }
+//
+// console.log(invertSign(10));
+//
+// function returnTwo (num) {
+//     return num === 2;
+// }
+//
+// console.log(returnTwo(2));
+
+
+function indentity (input) {
+    return
+}
