85f25b6f89a6e6f4f7f8400d5e3001aa76fa76c7
Jason Kempel <kempel.jason13@gmail.com>
2019-08-13 15:14:02 -0500

    array exercise

--- js/iterating.js
+++ js/iterating.js
@@ -6,30 +6,25 @@
      * Create an array of 4 people's names and store it in a variable called
      * 'names'.
      */
-
     /**
      * TODO:
      * Create a log statement that will log the number of elements in the names
      * array.
      */
-
     /**
      * TODO:
      * Create log statements that will print each of the names individually by
      * accessing each element's index.
      */
-
     /**
      * TODO:
      * Write some code that uses a for loop to log every item in the names
      * array.
      */
-
     /**
      * TODO:
      * Refactor your above code to use a `forEach` loop
      */
-
     /**
      * TODO:
      * Create the following three functions, each will accept an array and
@@ -43,5 +38,41 @@
      *  > second([1, 2, 3, 4, 5]) // returns 2
      *  > last([1, 2, 3, 4, 5]) // return 5
      */
+
+
+    var names = ["Mia", "Julian", "Jason Jr", "Jenevieve"];
+        console.log(names.length);
+
+        console.log(names[0]);
+        console.log(names[1]);
+        console.log(names[2]);
+        console.log(names[3]);
+
+    names.forEach(function(name) {
+        console.log(name);
+    });
+
+    for (var i = 0; i < names.length; i++) {
+        console.log ("The names of everyone in my family are: " + names[i]);
+    }
+
+
+    function first(array) {
+        return array[0];
+    }
+
+    function second(array) {
+        return array[1];
+    }
+
+    function third(array) {
+        return array[3];
+    }
+
+
+    console.log(first(names));
+    console.log(second(names));
+    console.log(third(names));
+
 
 })();
