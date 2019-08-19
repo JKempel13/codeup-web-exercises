cbeec43db4efac730ca488a9c38c48e025cb575c
Jason Kempel <kempel.jason13@gmail.com>
2019-08-19 09:19:03 -0500

    functions bonus

--- js/function-bonus.js
+++ js/function-bonus.js
@@ -1,28 +1,160 @@
 "use strict";
 
-// function isMultipleOf (target, n) {
-//     return target % n;
-// }
-//
-// console.log (isMultipleOf(6, 3));
-//
-// // function hasLowerCase (string) {
-// //     return
-// // }
-//
-// function invertSign (num) {
-//     return num * -1
-// }
-//
-// console.log(invertSign(10));
-//
-// function returnTwo (num) {
-//     return num === 2;
-// }
-//
-// console.log(returnTwo(2));
+// Make a function named isNumeric(input) that returns if the input is a number like 5 or 2.3 or a string containing only a number like "2".
+//     Example isNumeric(3) is - true. isNumeric("banana") should return false.
 
+//     function isNumeric(input) {
+//     if (input === "number") {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+//
+// console.log(isNumeric());
 
-function indentity (input) {
-    return
+// Make a function named isEqual(input1, input2) that returns if both inputs have the same value. areEqual
+
+// function isEqual(input1, input2) {
+//     if (input1 == input2) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+//
+// console.log(isEqual(2,2));
+
+// Make a function named isIdentical(input1, input2) that returns if both inputs are same value and data type.
+
+// function isIdentical(input1,input2) {
+//     if (input1 === input2) {
+//         return true;
+//     }
+// }
+//
+// console.log(isIdentical(1,1));
+
+// Make a function named not(input) returns the input with a flipped boolean
+
+// function not(input) {
+//     if (input === true) {
+//         return false;
+//     } else if (input === false) {
+//         return true;
+//     }
+// }
+//
+// console.log(not(false));
+
+// Make a function named isOdd(number) that returns true if the number is odd
+
+// function isOdd(number) {
+//     if (number % 2 !== 0) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+//
+// console.log(isOdd(31));
+
+// Make a function named isEven(number) that returns true if the number is even or zero
+// Make a function named isPositive(number) that returns true if the provided input is a positive number. False for zero or any other number or data type.
+
+// function isPositive(number) {
+//     if (number > 0) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+//
+// console.log(isPositive(29));
+
+// Make a function named isNegative(number) that returns true if the provided input is a negative number. False for zero or any other number or data type.
+// Make a function named identity(input) that returns the input exactly as provided.
+// Make a function named isFive(input)
+// Make a function named addFive(input) that adds five to some input.
+// Make a function named isMultipleOfFive(input)
+// Make a function named isThree(input)
+// Make a function named isMultipleOfThree(input)
+// Make a function named isMultipleOfThreeAndFive(input)
+// Make a function named isMultipleOf(target, n) which checks if target is evenly divisible by n
+// Make a function named isTrue(boolean)
+// Make a function named isFalse(boolean)
+// Make a function named isTruthy(input) // remember that values other than true will behave like true. you may need to lookup truthy/falsy values in JavaScript
+// Make a function named isFalsy(input) // remember that values other than false behave like false
+// Make a function named isVowel(letter)
+// Make a function named isConsonant(letter)
+// Make a function named isCapital(letter)
+// Make a function named isLowerCase(letter)
+// Make a function named hasLowerCase(string) that returns if a string has any lower cased
+// Make a function named isSpace(letter) that returns if a character is a space character
+// Make a function named isZero(number)
+// Make a function named notZero(input) that returns true if the input is any number except for zero.
+// Write a function named lowerCase(string)
+// Write a function named double(n) that returns a number times two
+// Write a function named triple(n) that returns a number times 3
+// Write a function named quadruple(n) that returns a number times 4
+// Write a function named half(n) that returns 1/2 of the provided input
+// Write a function named subtract(a, b) that returns a minus b
+// Write a function named multiply(a, b) that returns the product of a times b
+// Write a function named divide(a, b) that returns a divided by b
+// Write a function named remainder(a, b) that returns the remainder after dividing a by b
+// Make a function named modulo(a, b) that returns the returns the remainder after dividing a by b
+// Write a function named cube(n) that returns n * n * n
+// Write a function named squareRoot(n) that returns the square root of the input
+// Make a function named absoluteValue(number) that returns the absolute value of a number.
+// Make a function named isBlank(input) that determines if a given input is spaces, newline characters, or tabs.
+// Write a function named cubeRoot(n) that returns the cube root of the input
+// // Write a function named invertSign(number) that returns a negative version of a postive number, a positve version of negative, and false for all else.
+//
+// function invertSign(number) {
+//     if (number > 0) {
+//         return -number;
+//     } else if (number < 0) {
+//         return Math.abs(number);
+//     } else {
+//         return false;
+//     }
+// }
+//
+// console.log(invertSign(-100));
+
+// Write a function named degreesToRadians(number)
+// Write a function named radiansToDegrees(number)
+// Make a function named trim(string) that removes empty spaces before and after the input.
+
+// function trim(string) {
+//     return string.trim();
+// }
+//
+// console.log(trim("Jason"));
+
+// Make a function named notNot(input) that the negation of the negation of the input.
+// Make a function named and(predicate1, predicate2) that returns the logical operation of AND
+// Make a function named or(predicate1, predicate2) that returns the logical operation of OR
+
+function or(predicate1, predicate2) {
+
 }
+
+// // Write a function called reverseString(string) that reverses a string
+//
+// function reverseString(string) {
+//     var backwards = string.split("");
+//     var reverseBackwards = backwards.reverse();
+//     var joinString = reverseBackwards.join("");
+//     return joinString;
+// }
+//
+// console.log(reverseString("Jason"));
+
+// Make a function named rollDice(sides) that takes in an argument containing the number of sides the die should have. Generate a random number between 1 up to and - including the number of sides.
+
+// function rollDice(sides) {
+//     return (Math.floor(Math.random () * 6) +1);
+// }
+//
+// console.log(rollDice());
