d08adf4f6c2285a1ea573a1cbf3de4fd06a9eba3
Jason Kempel <kempel.jason13@gmail.com>
2019-09-03 10:57:55 -0500

    commiting work

--- js/weather-map.js
+++ js/weather-map.js
@@ -1,37 +1,39 @@
 "use strict";
 $(document).ready(function() {
     //======================== cycle through 3 days =========================
-    // $.get("https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/" + darkSkyToken + "/29.4241,-98.4936").done(function (data) {
+
         $('#update-map-btn').click(function () {
             var inputLat = $('#lat').val();
             var inputLon = $('#lon').val();
-            $.get('https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/' + darkSkyToken + "/" + inputLat + ',' + inputLon).done(function (data) {
-                console.log(data.daily);
-                var forecast = "";
-                $('#blah').html(function () {
-                    for (var i = 0; i < 3; i++) {
-                        var highTemp = data.daily.data[i].apparentTemperatureHigh + '<span>&#176;</span>';
-                        var lowTemp = data.daily.data[i].apparentTemperatureLow + '<span>&#176;</span>';
-                        var weatherIcon = data.daily.data[i].icon;
-                        var humidity = data.daily.data[i].humidity;
-                        var windSpeed = data.daily.data[i].windSpeed;
-                        var pressure = data.daily.data[i].pressure;
-                        var dateObject = new Date(data.daily.data[i].sunriseTime * 1000);
-                        var n = dateObject.toDateString();
-                        forecast += "<div class='card col-4'>";
-                        forecast += "<h4>" + highTemp + "/" + lowTemp + "</h4>";
-                        forecast += "<img src='" + displayIconPicture(iconsArr, weatherIcon) + "'>";
-                        forecast += "<p>" + weatherIcon + "</p>";
-                        forecast += "<p><span style='font-weight: bold'>Humidity: </span>" + humidity + "</p>";
-                        forecast += "<p><span style='font-weight: bold'>Wind: </span>" + windSpeed + "</p>";
-                        forecast += "<p><span style='font-weight: bold'>Pressure: </span>" + pressure + "</p>";
-                        forecast += "<p><span style='font-weight: bold'>Date: </span>" + n + "</p>";
-                        forecast += "</div>";
-                    }
-                    return forecast;
-                });
-            })
+        $.get('https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/' + darkSkyToken + "/" + inputLat + ',' + inputLon).done(function (data) {
+
+            var forecast = "";
+            $('#blah').html(function () {
+                for (var i = 0; i < 3; i++) {
+                    var highTemp = data.daily.data[i].apparentTemperatureHigh + '<span>&#176;</span>';
+                    var lowTemp = data.daily.data[i].apparentTemperatureLow + '<span>&#176;</span>';
+                    var weatherIcon = data.daily.data[i].icon;
+                    var humidity = data.daily.data[i].humidity;
+                    var windSpeed = data.daily.data[i].windSpeed;
+                    var pressure = data.daily.data[i].pressure;
+                    var dateObject = new Date(data.daily.data[i].sunriseTime * 1000);
+                    var n = dateObject.toDateString();
+
+                    forecast += "<div class='card col-4'>";
+                    forecast += "<h4>" + highTemp + "/" + lowTemp + "</h4>";
+                    forecast += "<img src='" + displayIconPicture(iconsArr, weatherIcon) + "'>";
+                    forecast += "<p>" + weatherIcon + "</p>";
+                    forecast += "<p><span style='font-weight: bold'>Humidity: </span>" + humidity + "</p>";
+                    forecast += "<p><span style='font-weight: bold'>Wind: </span>" + windSpeed + "</p>";
+                    forecast += "<p><span style='font-weight: bold'>Pressure: </span>" + pressure + "</p>";
+                    forecast += "<p><span style='font-weight: bold'>Date: </span>" + n + "</p>";
+                    forecast += "</div>";
+                }
+                return forecast;
+            });
         });
+        });
+
         //============= weather icon array function=======================
         var iconsArr = [
             {
@@ -100,13 +102,44 @@ $(document).ready(function() {
         })
             .setLngLat([-98.4916, 29.4252])
             .addTo(map);
-    function onDragEnd() {
-        var lngLat = marker.getLngLat();
-        coordinates.style.display = 'block';
-        coordinates.innerHTML = 'Longitude: ' + lngLat.lng + '<br />Latitude: ' + lngLat.lat;
-    }
+
+        function onDragEnd() {
+            var lngLat = marker.getLngLat();
+            coordinates.style.display = 'block';
+            coordinates.innerHTML = 'Longitude: ' + lngLat.lng + '<br />Latitude: ' + lngLat.lat;
+            var latitude = lngLat.lat.toString();
+            var longitude = lngLat.lng.toString();
+            $.get('https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/' + darkSkyToken + "/" + latitude + ',' + longitude).done(function (data) {
+
+                var forecast = "";
+                $('#blah').html(function () {
+                    for (var i = 0; i < 3; i++) {
+                        var highTemp = data.daily.data[i].apparentTemperatureHigh + '<span>&#176;</span>';
+                        var lowTemp = data.daily.data[i].apparentTemperatureLow + '<span>&#176;</span>';
+                        var weatherIcon = data.daily.data[i].icon;
+                        var humidity = data.daily.data[i].humidity;
+                        var windSpeed = data.daily.data[i].windSpeed;
+                        var pressure = data.daily.data[i].pressure;
+                        var dateObject = new Date(data.daily.data[i].sunriseTime * 1000);
+                        var n = dateObject.toDateString();
+
+                        forecast += "<div class='card col-4'>";
+                        forecast += "<h4>" + highTemp + "/" + lowTemp + "</h4>";
+                        forecast += "<img src='" + displayIconPicture(iconsArr, weatherIcon) + "'>";
+                        forecast += "<p>" + weatherIcon + "</p>";
+                        forecast += "<p><span style='font-weight: bold'>Humidity: </span>" + humidity + "</p>";
+                        forecast += "<p><span style='font-weight: bold'>Wind: </span>" + windSpeed + "</p>";
+                        forecast += "<p><span style='font-weight: bold'>Pressure: </span>" + pressure + "</p>";
+                        forecast += "<p><span style='font-weight: bold'>Date: </span>" + n + "</p>";
+                        forecast += "</div>";
+                    }
+                    return forecast;
+                });
+            });
+        }
+
         marker.on('dragend', onDragEnd);
         onDragEnd();
-        // })
-    // })
+
 });
+
