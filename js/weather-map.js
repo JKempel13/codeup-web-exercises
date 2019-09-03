077590245ba981988b5ae06dc9c218992a8c79fe
Jason Kempel <kempel.jason13@gmail.com>
2019-09-03 09:59:20 -0500

    weather-map, no adjustments

--- js/weather-map.js
+++ js/weather-map.js
@@ -1,13 +1,12 @@
 "use strict";
 $(document).ready(function() {
     //======================== cycle through 3 days =========================
-    $.get("https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/" + darkSkyToken + "/29.4241,-98.4936").done(function (data) {
+    // $.get("https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/" + darkSkyToken + "/29.4241,-98.4936").done(function (data) {
         $('#update-map-btn').click(function () {
             var inputLat = $('#lat').val();
             var inputLon = $('#lon').val();
             $.get('https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/' + darkSkyToken + "/" + inputLat + ',' + inputLon).done(function (data) {
                 console.log(data.daily);
-
                 var forecast = "";
                 $('#blah').html(function () {
                     for (var i = 0; i < 3; i++) {
@@ -19,7 +18,6 @@ $(document).ready(function() {
                         var pressure = data.daily.data[i].pressure;
                         var dateObject = new Date(data.daily.data[i].sunriseTime * 1000);
                         var n = dateObject.toDateString();
-
                         forecast += "<div class='card col-4'>";
                         forecast += "<h4>" + highTemp + "/" + lowTemp + "</h4>";
                         forecast += "<img src='" + displayIconPicture(iconsArr, weatherIcon) + "'>";
@@ -31,15 +29,11 @@ $(document).ready(function() {
                         forecast += "</div>";
                     }
                     return forecast;
-
                 });
             })
         });
-
         //============= weather icon array function=======================
-
         var iconsArr = [
-
             {
                 link: "img/SVG/Sun.svg",
                 name: "clear-day"
@@ -81,7 +75,6 @@ $(document).ready(function() {
                 name: "partly-cloudy-night"
             }
         ];
-
         //============== display icons =================
         function displayIconPicture(iconsArr, weatherIcon) {
             for (var j = 0; j < iconsArr.length; j++) {
@@ -90,9 +83,7 @@ $(document).ready(function() {
                 }
             }
         }
-
         //====================== my map ====================
-
         mapboxgl.accessToken = mapboxToken;
         var map = new mapboxgl.Map({
             container: 'map', // container id
@@ -104,19 +95,18 @@ $(document).ready(function() {
             accessToken: mapboxgl.accessToken,
             mapboxgl: mapboxgl
         }));
-
         var marker = new mapboxgl.Marker({
             draggable: true
         })
             .setLngLat([-98.4916, 29.4252])
             .addTo(map);
-
-        function onDragEnd() {
-            var lngLat = marker.getLngLat();
-            coordinates.style.display = 'block';
-            coordinates.innerHTML = 'Longitude: ' + lngLat.lng + '<br />Latitude: ' + lngLat.lat;
-        }
+    function onDragEnd() {
+        var lngLat = marker.getLngLat();
+        coordinates.style.display = 'block';
+        coordinates.innerHTML = 'Longitude: ' + lngLat.lng + '<br />Latitude: ' + lngLat.lat;
+    }
         marker.on('dragend', onDragEnd);
+        onDragEnd();
         // })
-    })
+    // })
 });
