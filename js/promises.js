ce9f2984842f953d8852400725b64e7f0f53a520
Jason Kempel <kempel.jason13@gmail.com>
2019-09-10 20:10:45 -0500

    corrected name to jquery_exercises

--- js/promises.js
+++ js/promises.js
@@ -53,17 +53,34 @@
 // Add an input field and submit button (you will need to prevent the default behaviour) that when clicked, runs the following function...
 // Create a function that takes in a username and fetch's the color preference based on the username and sets the background color to that color.
 
-function profileColor () {
-    return fetch('./data/profiles.json').then(response => {
-        return response.json();
-    }).then(data => console.log(data))
-}
-profileColor();
+
+
+// function profileColor () {
+//     return fetch('./data/profiles.json').then(response => {
+//         return response.json();
+//     }).then(data => console.log(data))
+// }
+// profileColor();
+
+
 
 // -- Ex. 2
 // Using the Star Wars API, log the first film a Star Wars character's homeworld appeared in
+
+// fetch('https://swapi.co/api/people').then(response => {
+//     return response.json();
+// }).then(data => {
+//     console.log(data.results);
+//     for(let i = 0;i < data.results.length; i++){
+//
+//     }
+// });
+
 // -- Ex. 3
 // Using the GitHub API and reduce(), find the average hour of the last 3 pushes. Ignore minutes.
+
+
+
 // -- Ex. 4
 // Create star-chars.json file in your data folder and paste in the following...
 // [
