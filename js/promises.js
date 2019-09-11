5fa4238c22dc1569fe78d7f07624d532d15cad60
Jason Kempel <kempel.jason13@gmail.com>
2019-09-10 23:00:29 -0500

    uncommented exercises

--- js/promises.js
+++ js/promises.js
@@ -2,24 +2,24 @@
 
 // // Write a function named wait that accepts a number as a parameter, and returns a promise that resolves after the passed number of milliseconds.
 //
-// const wait = time => {
-//     return new Promise((resolve) => {
-//         // ========== my result ===============
-//         setTimeout(() => {
-//             resolve();
-//         },time);
-//     //    ========= Justin's result ============
-//     //     setTimeout(resolve,time);
-//     })
-// };
-// // wait(1000).then(() => console.log('You\'ll see this after 1 second'));
-// // wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
+const wait = time => {
+    return new Promise((resolve) => {
+        // ========== my result ===============
+        // setTimeout(() => {
+        //     resolve();
+        // },time);
+    //    ========= Justin's result ============
+        setTimeout(resolve,time);
+    })
+};
+// wait(1000).then(() => console.log('You\'ll see this after 1 second'));
+// wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
 //
-// fetch('https://swapi.co/api/people/1').then(response => {
-//     return response.json();
-// }).then((data) => {
-//     // console.log(data);
-// });
+fetch('https://swapi.co/api/people/1').then(response => {
+    return response.json();
+}).then((data) => {
+    // console.log(data);
+});
 //
 // Promise.resolve("string1").then(string => {
 //     // console.log(string);
@@ -34,12 +34,12 @@
 // // string2
 // // string3
 
-// function getUserNameFromGithub () {
-//     return fetch('https://api.github.com/users/JKempel13/events/public', {headers: {'Authorization': `token ${githubToken}`}})
-//         .then(response => response.json())
-//         .then(data => console.log(data[0].created_at))
-// }
-// getUserNameFromGithub();
+function getUserNameFromGithub () {
+    return fetch('https://api.github.com/users/JKempel13/events/public', {headers: {'Authorization': `token ${githubToken}`}})
+        .then(response => response.json())
+        .then(data => console.log(data[0].created_at))
+}
+getUserNameFromGithub();
 
 
 // =================== Fetch Bonuses
