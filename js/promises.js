b590335a2dc47950af4f146edb8e8c2dcc2bbc4e
Jason Kempel <kempel.jason13@gmail.com>
2019-09-10 19:30:47 -0500

    promises bonuses

--- js/promises.js
+++ js/promises.js
@@ -34,16 +34,55 @@
 // // string2
 // // string3
 
-function getUserNameFromGithub () {
-    return fetch('https://api.github.com/users/JKempel13/events/public', {headers: {'Authorization': `token ${githubToken}`}}).then(response => {
+// function getUserNameFromGithub () {
+//     return fetch('https://api.github.com/users/JKempel13/events/public', {headers: {'Authorization': `token ${githubToken}`}})
+//         .then(response => response.json())
+//         .then(data => console.log(data[0].created_at))
+// }
+// getUserNameFromGithub();
+
+
+// =================== Fetch Bonuses
+// -- Ex. 1
+// In your data directory, create a profiles.json file with the following...
+// {
+//     "sandy123": "brown",
+//     "bob234": "green",
+//     "sally345": "blue"
+// }
+// Add an input field and submit button (you will need to prevent the default behaviour) that when clicked, runs the following function...
+// Create a function that takes in a username and fetch's the color preference based on the username and sets the background color to that color.
+
+function profileColor () {
+    return fetch('./data/profiles.json').then(response => {
         return response.json();
-    }).then(data => {
-        console.log(data[0].created_at);
-    })
+    }).then(data => console.log(data))
 }
-getUserNameFromGithub();
+profileColor();
 
-
+// -- Ex. 2
+// Using the Star Wars API, log the first film a Star Wars character's homeworld appeared in
+// -- Ex. 3
+// Using the GitHub API and reduce(), find the average hour of the last 3 pushes. Ignore minutes.
+// -- Ex. 4
+// Create star-chars.json file in your data folder and paste in the following...
+// [
+//     {
+//         "name": "Rey",
+//         "coolness": 9
+//     },
+//     {
+//         "name": "Luke Skywalker",
+//         "coolness": 9
+//     },
+//     {
+//         "name": "Darth Vadar",
+//         "coolness": 10
+//     }
+// ]
+// Create a function that fetches this array and returns an array of homeworlds for the characters.
+//     It will look something like the following...
+// ['Jakku', 'Tatooine', 'Tatooine']
 
 
 
