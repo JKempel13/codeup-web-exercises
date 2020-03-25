de2ac053e1c06c5a09f7d61d1429d1bd37a8762b
Jason Kempel <kempel.jason13@gmail.com>
2020-03-25 12:53:19 -0500

    trying to change between tabs on new project

--- js/new-project.js
+++ js/new-project.js
@@ -11,8 +11,21 @@
     }
 
     $(document).ready(function(){
-        $('.btn1').click(function(){
-            $(this).show();
-        })
-    })
+
+        $('.tabs_navigation').click(function(event) {
+            event.preventDefault();
+
+            // Toggle active class on tab buttons
+            $(this).parent().addClass("current");
+            $(this).parent().siblings().removeClass("current");
+
+            // display only active tab content
+            var activeTab = $(this).attr("p");
+            $('.single-tab').not(activeTab).css("display","none");
+            $(activeTab).fadeIn();
+
+        });
+
+    });
+
 })();
