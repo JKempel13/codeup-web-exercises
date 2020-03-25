8124a21346070a89c703f6d42053f35e61727a4e
Jason Kempel <kempel.jason13@gmail.com>
2020-03-25 13:12:49 -0500

    got the tabs working

--- js/new-project.js
+++ js/new-project.js
@@ -10,22 +10,22 @@
         document.getElementById(tab).style.display = "block";
     }
 
-    $(document).ready(function(){
-
-        $('.tabs_navigation').click(function(event) {
-            event.preventDefault();
-
-            // Toggle active class on tab buttons
-            $(this).parent().addClass("current");
-            $(this).parent().siblings().removeClass("current");
-
-            // display only active tab content
-            var activeTab = $(this).attr("p");
-            $('.single-tab').not(activeTab).css("display","none");
-            $(activeTab).fadeIn();
-
-        });
-
-    });
+    // $(document).ready(function(){
+    //
+    //     $('.tabs_navigation').click(function(event) {
+    //         event.preventDefault();
+    //
+    //         // Toggle active class on tab buttons
+    //         $(this).parent().addClass("current");
+    //         $(this).parent().siblings().removeClass("current");
+    //
+    //         // display only active tab content
+    //         var activeTab = $(this).attr("p");
+    //         $('.single-tab').not(activeTab).css("display","none");
+    //         $(activeTab).fadeIn();
+    //
+    //     });
+    //
+    // });
 
 })();
